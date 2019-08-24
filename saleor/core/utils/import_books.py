152ee27ab2eb93ac45839f3761d5c54b903f5da8
hupratt <hp@hp.com>
2019-08-24 21:11:30 +0200

    add custom script

--- saleor/core/utils/import_books.py
+++ saleor/core/utils/import_books.py
@@ -4,21 +4,35 @@ import os
 from django.core.files import File
 from saleor.product.thumbnails import create_product_thumbnails
 
-image_name = "saleordemoproduct_paints_01.png"
-image_dir = "saleor/static/placeholders/"
-
 
 def get_image(image_dir, image_name):
     img_path = os.path.join(image_dir, image_name)
     return File(open(img_path, "rb"), name=image_name)
 
+# name = "Um Museu do Outro Mundo"
+name = "Paula Rego"
+price = '10.00'
+# image_name = "saleordemoproduct_paints_01.png"
+# image_name ="Um Museu do Outro Mundo.jpg"
+image_name = "Paula Rego.jpg"
+image_dir = "saleor/static/placeholders/"
+isbn = 9789899568792
+# 1 for English attr = {"1": ["1"]}
+# 2 for PT attr = {"1": ["2"]}
+attr = {"1": ["2"]}
+weight = "0.1"
+pk = 1
+quantity = 1
 
-defaults = {"name":"try out", "weight": "1.0", "category_id": 2, "product_type_id": 2, "attributes": "{}", "price": Money('15.00','EUR')}
-product, _ = Product.objects.update_or_create(pk=1, defaults=defaults)
+def create_product(quantity = quantity, name = name, price = price, weight = weight, image_name = image_name, image_dir = image_dir, isbn = isbn, attr = attr, pk = pk):
+    product_defaults = {"name":name, "weight": weight, "category_id": 2, "product_type_id": 2, "attributes": "{}", "price": Money(price,'EUR')}
+    product, _ = Product.objects.update_or_create(pk = pk, defaults=product_defaults)
+    product_image = ProductImage(product=product, image=get_image(image_dir, image_name))
+    product_image.save()
+    create_product_thumbnails.delay(product_image.pk)
+    variant_defaults = {"name":name, "weight": weight, "product_id": pk, "attributes": attr, "price_override": Money('15.00','EUR'), "cost_price": Money('1.00','EUR'), "sku":isbn, "quantity": quantity}
+    ProductVariant.objects.update_or_create(pk=pk, defaults=variant_defaults)
 
-product_image = ProductImage(product=product, image=get_image(image_dir, image_name))
-product_image.save()
-create_product_thumbnails.delay(product_image.pk)
-
-defaults = {"name":"try out", "weight": "1.0", "product_id": 1, "attributes": "{\"1\": [\"1\"]}", "price_override": Money('15.00','EUR'), "cost_price": Money('1.00','EUR')}
-ProductVariant.objects.update_or_create(pk=1, defaults=defaults)
+create_product(name = name, price = price, weight = weight, 
+               image_name = image_name, image_dir = image_dir, 
+               isbn = isbn, attr = attr, pk = pk, quantity = quantity)
